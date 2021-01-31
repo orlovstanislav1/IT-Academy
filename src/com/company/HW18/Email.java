80104cf865ba8320f1e62277fc257fea0825d717
orlovstanislav1 <orlovstanislav1@rambler.ru>
2021-01-31 15:51:54 +0300

    2

---
+++ src/com/company/HW18/Email.java
@@ -0,0 +1,68 @@
+package com.company.HW18;
+
+public class Email {
+    private int id;
+    private String to;
+    private String from;
+    private String heading;
+    private String body;
+
+    public Email(int id, String to, String from, String heading, String body) {
+        this.id = id;
+        this.to = to;
+        this.from = from;
+        this.heading = heading;
+        this.body = body;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getHeading() {
+        return heading;
+    }
+
+    public void setHeading(String heading) {
+        this.heading = heading;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    @Override
+    public String toString() {
+        return "Email{" +
+                "id=" + id +
+                ", to='" + to + '\'' +
+                ", from='" + from + '\'' +
+                ", heading='" + heading + '\'' +
+                ", body='" + body + '\'' +
+                '}';
+    }
+}
